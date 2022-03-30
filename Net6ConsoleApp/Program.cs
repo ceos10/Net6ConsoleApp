8885c4d380c4bffb9a46d0f7310cdb924d135528
carlosocrospoma <carlos94101@hotmail.com>
2022-03-29 23:53:49 -0500

    Adding some exercises

--- Net6ConsoleApp/Program.cs
+++ Net6ConsoleApp/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World Net6!");
+
+//7 awesome improvements for LINQ in .NET 6
+
+//1
+var names = new[] { "Nick", "Mike", "John", "Leyla", "David", "Damian" };
+var chunked = names.Chunk(3);
+
+//2
+var firstSet = new[] { "Nick Chapsas" };
+var secondSet = new[] { "Peter Chapsas" };
+var thirdSet = new[] { "Maria Chapsas" };
+
+var allNames = firstSet.Concat(secondSet).Concat(thirdSet);
+
+if (allNames.TryGetNonEnumeratedCount(out var count))
+{
+
+}
+
+//3
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Net6ConsoleApp
 Net 6 Console Application
+
+https://www.youtube.com/watch?v=sIXKpyhxHR8&list=LL&index=1&t=543s
